7199d56fbb19d920b645432933942686c92c8431
Mammasse Amine <aminegolden2016@gmail.com>
2021-10-19 20:44:15 +0200

    add rReact Helmet for SEO

--- src/components/selectLanguage/SelectLanguage.jsx
+++ src/components/selectLanguage/SelectLanguage.jsx
@@ -1,4 +1,4 @@
-import {useState, useEffect}  from 'react';
+import {useState}  from 'react';
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
