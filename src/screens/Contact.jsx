fd0f1469a4f51316cab99613d5d60a33c7633be6
Mammasse Amine <aminegolden2016@gmail.com>
2021-10-29 18:56:05 +0200

    Contact Form Valdation

--- src/screens/Contact.jsx
+++ src/screens/Contact.jsx
@@ -23,11 +23,28 @@ import "./Contact.css";
 // Translation
 import {useTranslation} from 'react-i18next'
 
-
+// Form Validation
+import {Formik,Form,Field,ErrorMessage} from "formik"
+import * as Yup from "yup"
 
 const Contact = ()=>{
 
-  const {t} = useTranslation()
+  const {t} = useTranslation();
+
+  const initialValues = {Fname : "", Lname : "", Email : "", Mobile : "", Msg : ""};
+
+  const validationSchema = Yup.object().shape({
+    Fname: Yup.string().required(t("rqFname")).matches(/^[^0-9]+$/, t("strFname")).min(3,t("minFname")),
+    Lname : Yup.string().required(t("rqLname")).matches(/^[^0-9]+$/, t("strLname")).min(3,t("minLname")),
+    Email : Yup.string().email(t("Vemail")).required(t("rqEmail")),
+    Mobile : Yup.number(t("Vmobile")).positive(t("Vmobile")).integer(t("Vmobile")).min(100000000,t("Vmobile")),
+    Msg : Yup.string().required(t("rqMsg")).min(10,t("Vmsg"))
+  }) 
+
+  const onSubmit = (values,props)=>{
+    alert(JSON.stringify(values), null,2);
+    props.resetForm()
+  }
 
   return(
 
@@ -101,25 +118,34 @@ const Contact = ()=>{
       </Paper>
       <Box className="contactForm">
         <h2>{t("Touch")}</h2>
-          <div className="formBox">
-              <div className="inputBox w50">
-              <TextField type="text" label={t("First Name")} variant="standard" required fullWidth/>
-              </div>
-              <div className="inputBox w50">
-              <TextField type="text" label={t("Last Name")} variant="standard" required fullWidth/>
-              </div>
-              <div className="inputBox w50">
-              <TextField  type="email" label={t("Email")} variant="standard" required fullWidth/>
-              </div>
-              <div className="inputBox w50">
-              <TextField  type="text" label={t("Mobile Number")}variant="standard" fullWidth/>
-              </div>
+        
+          <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+            {(props)=>( 
 
-              <div className="inputBox w100">
-              <TextField label={t("Your Message Here")} rows={4} multiline  variant="standard" required className="textarea"/>
-              </div> 
-              <Button type="submit" variant="contained" endIcon={<MdSend />}>{t("Send")} </Button>
-            </div>
+              <Form >
+                  <div className="formBox">
+                  <div className="inputBox w50">
+                  <Field as={TextField} name="Fname" type="text" label={t("First Name")} variant="standard"  fullWidth helperText={<ErrorMessage name="Fname"/>} required error={props.errors.Fname && props.touched.Fname}  />
+                  </div>
+                  <div className="inputBox w50">
+                  <Field as={TextField} type="text" name="Lname" label={t("Last Name")} variant="standard"  fullWidth  helperText={<ErrorMessage name="Lname"/>} required error={props.errors.Lname && props.touched.Lname}/>
+                  </div>
+                  <div className="inputBox w50">
+                  <Field as={TextField} name="Email" type="email" label={t("Email")} variant="standard"  fullWidth  helperText={<ErrorMessage name="Email"/>} required error={props.errors.Email && props.touched.Email} />
+                  </div>
+                  <div className="inputBox w50">
+                  <Field as={TextField} name="Mobile"  type="text" label={t("Mobile Number")}variant="standard" fullWidth helperText={<ErrorMessage name="Mobile"/>} error={props.errors.Mobile && props.touched.Mobile} />
+                  </div>
+
+                  <div className="inputBox w100">
+                  <Field as={TextField} name="Msg" label={t("Your Message Here")} rows={3} multiline  variant="standard"  className="textarea" helperText={<ErrorMessage name="Msg"/>} required error={props.errors.Msg && props.touched.Msg}/>
+                  </div> 
+                  <Button sx={{mt:-3}} type="submit" variant="contained" endIcon={<MdSend />}>{t("Send")} </Button>
+                </div>
+              </Form>
+
+            )}
+          </Formik>
       </Box>
 
 
